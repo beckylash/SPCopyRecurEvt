5a01ab0f383779efde956504521be66de8225aa8
Epic Trends (r) <rebeccatlash@gmail.com>
2017-07-16 14:10:50 -0500

    Add files via upload

---
+++ ETCopyEvent/Features/ET.CopyRecurEvt/ET.CopyRecurEvt.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="54aa8cd2-4f87-42e4-b889-22e36f03d42b" description="Deploys list event receiver and solves issue of copying calendar events with recurrence to a different site or site collection.&#xD;&#xA;" featureId="54aa8cd2-4f87-42e4-b889-22e36f03d42b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ET Copy Recurring Event" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="72f669b9-fa47-4281-89f9-5ee0cb94d71b" />
+  </projectItems>
+</feature>
---
+++ ETCopyEvent/RecurringEventAdded/RecurringEventAdded.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace MACCopyEvent.RecurringEventAdded
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class RecurringEventAdded : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was added.
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+            using (SPSite site = properties.OpenSite())
+            { 
+            using (SPWeb web = site.OpenWeb())
+                {
+                    var destinationWeb = "http://becky.aepdev.com/sites/ET/Test1/cal1/cal3/";
+
+                    using (SPSite destSite = new SPSite(destinationWeb))
+                    {
+                        using (SPWeb destWeb = destSite.OpenWeb())
+                        { 
+
+                        var list = destWeb.Lists["cal4"];
+                        var itemPromote = list.Items.Add();
+                        itemPromote["Title"] = properties.ListItem["Title"];
+                        itemPromote["RecurrenceData"] = properties.ListItem["RecurrenceData"];
+                        itemPromote["EventType"] = properties.ListItem["EventType"];
+                        itemPromote["EventDate"] = properties.ListItem["EventDate"];
+                        itemPromote["EndDate"] = properties.ListItem["EndDate"];
+                        itemPromote["UID"] = System.Guid.NewGuid();
+                        itemPromote["TimeZone"] = 13;
+                        itemPromote["Recurrence"] = -1;
+                        itemPromote.Update();
+                    }
+                    }
+            }
+            }
+        }
+
+
+    }
+}
